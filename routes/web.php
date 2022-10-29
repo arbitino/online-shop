174192b1d95341a51c933eb5335014078cbc0f7b
= <=>
2022-10-29 17:03:37 +0300

    добавли Ñхелперы, реализоавные флеш уведомленичя

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::get('/login', 'login')->name('login');
-    Route::post('/login', 'signIn')->name('signIn');
+
+    Route::post('/login', 'signIn')
+        ->middleware('throttle:auth')
+        ->name('signIn');
 
     Route::delete('/logout', 'logout')->name('logout');
 
     Route::get('/register', 'register')->name('register');
-    Route::post('/register', 'signUp')->name('signUp');
+
+    Route::post('/register', 'signUp')
+        ->middleware('throttle:auth')
+        ->name('signUp');
 
     Route::get('/reset/{token}', 'reset')
         ->middleware('guest')
